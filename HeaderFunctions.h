e8c2bc2d96f014fc7cb8a7103d920fa2598974bb
ariostokuit <32111541+ariostokuit@users.noreply.github.com>
2019-07-22 17:19:49 -0700

    Add files via upload

---
+++ HeaderFunctions.h
@@ -0,0 +1,13 @@
+//
+//  HeaderFunctions.h
+//  Stats_with_Pointers
+//
+//  Created by Ariosto Kuit on 7/22/19.
+//  Copyright © 2019 Ariosto Kuit. All rights reserved.
+//
+
+#ifndef HeaderFunctions_h
+#define HeaderFunctions_h
+
+
+#endif /* HeaderFunctions_h */
